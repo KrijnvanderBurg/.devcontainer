28c622ad530c50eb4b74caa11c84ca6bec5e2be5
Krijn van der Burg <krijnvdburg@protonmail.com>
2025-04-17 22:55:03 +0200

    working spark

---
+++ .devcontainer/spark/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Submit a PySpark job to the cluster with more specific settings
+/opt/spark/bin/spark-submit \
+  --master spark://spark-master:7077 \
+  --deploy-mode client \
+  --conf spark.executor.memory=1g \
+  --conf spark.executor.cores=1 \
+  --conf spark.driver.host=dev-environment \
+  --conf spark.driver.bindAddress=0.0.0.0 \
+  jobbie.py
+
+echo "Spark job completed. Check the output above."
